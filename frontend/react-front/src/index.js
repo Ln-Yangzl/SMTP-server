daee857d75a254627eb266640bcfe1e1337e6aac
zlyang <565213263@qq.com>
2021-10-28 08:05:25 +0800

    init frontend react app

---
+++ frontend/react-front/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+
+
+function App(){
+    return(
+        <div>hello SMTP</div>
+    )
+}
+
+ReactDOM.render(
+      <App />,
+      document.getElementById('root')
+    );
