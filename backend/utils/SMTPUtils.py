57ee2c88bb37bd51b0fc565efc8e71e16a478866
Ln-Yangzl <565213263@qq.com>
2021-11-12 23:00:55 +0800

    fix SMTPServerError

--- backend/utils/SMTPUtils.py
+++ backend/utils/SMTPUtils.py
@@ -2,6 +2,8 @@ class SMTPServerError(Exception):
     def __init__(self, value) -> None:
         self.value = value
 
+    def getMessage(self) -> str:
+        return self.value
 
     def __str__(self) -> str:
         return repr(self.value)
